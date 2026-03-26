0da497c2f4f346926899eeb385afb39b652a1d83
Muhammad Ilyas <danish.ilyas786@gmail.com>
2026-03-26 16:11:43 +0500

    double stamp

--- resources/views/website/include/news.blade.php
+++ resources/views/website/include/news.blade.php
@@ -1,15 +1,48 @@
+<style>
+    .news-ticker-wrapper {
+        overflow: hidden;
+        white-space: nowrap;
+        width: 100%;
+        position: relative;
+    }
+
+    .news-ticker-content {
+        display: inline-block;
+        white-space: nowrap;
+        animation: ticker-scroll 40s linear infinite;
+    }
+
+    .news-ticker-content:hover {
+        animation-play-state: paused;
+    }
+
+    .news-ticker-item {
+        display: inline-block;
+        padding-left: 3rem;
+        font-size: 23px;
+        font-weight: bold;
+    }
+
+    @keyframes ticker-scroll {
+        0% { transform: translateX(-100%); }
+        100% { transform: translateX(100%); }
+    }
+</style>
+
 <div class="container-fluid content">
     @if (isset($activeNews) && $activeNews->isNotEmpty())
     <section class="content-header">
-      <div class="row" style="background-color: #ff0000; padding: 10px; border-radius: 5px;">
+      <div class="row" style="background-color: #ff0000; padding: 10px; border-radius: 5px; color:white;">
           <div class="col-lg-12">
-              <marquee class="mt-2 pb-2" style="color:white; line-height: 1.8; padding-top: 8px;" direction="right">
-                @foreach ($activeNews as $news)
-                  <b style="font-size:23px">
-                        {{$news->name}}
-                  </b>
-                  @endforeach
-              </marquee>
+              <div class="news-ticker-wrapper mt-2 pb-2" style="line-height: 1.8; padding-top: 8px;">
+                  <div class="news-ticker-content">
+                    @foreach ($activeNews as $news)
+                        <div class="news-ticker-item">
+                            {{$news->name}}
+                        </div>
+                    @endforeach
+                  </div>
+              </div>
           </div>
       </div>
     </section>
