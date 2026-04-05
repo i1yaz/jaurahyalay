fb3be720b3d55ab4da2770b2d8e546d025e482e9
Muhammad Ilyas <danish.ilyas786@gmail.com>
2026-04-05 22:04:24 +0500

    guest user

--- config/settings.php
+++ config/settings.php
@@ -2,8 +2,8 @@
 
     return [
         'contact' => [
-            'name' => 'CH ALI RAZA CHEEMA',
-            'phone' => '+92-321-3760-786',
+            'name' => 'Ilyas',
+            'phone' => '+92-313-780-9469',
             'email' => ''
         ],
         'profile_pic_type' => 'square'// square, circle, rectangle
--- resources/views/website/include/footer.blade.php
+++ resources/views/website/include/footer.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid content">
     <div id="footer">
-        {{-- <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-dark bg-dark">
             <span class="navbar-text text-white">&copy;@php echo date('Y'); @endphp Developed by
                 <a href="https://wa.me/923137809469" target="_blank"><b>Muhammad Ilyas</b>
                 </a></span>
-        </nav> --}}
+        </nav>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.bundle.min.js"></script>
