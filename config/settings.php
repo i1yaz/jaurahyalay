9a851c558c0bf59a5f33d05ea9978c2b198419b9
Muhammad Ilyas <danish.ilyas786@gmail.com>
2026-05-23 01:16:26 +0500

    change phone nu,ber

--- config/settings.php
+++ config/settings.php
@@ -1,10 +1,10 @@
 <?php
 
-    return [
-        'contact' => [
-            'name' => 'Ilyas',
-            'phone' => '+92-313-780-9469',
-            'email' => ''
-        ],
-        'profile_pic_type' => 'square'// square, circle, rectangle
-    ];
+return [
+    'contact' => [
+        'name' => 'Ch Shahbaz Dogga',
+        'phone' => '+45 42 75 83 21',
+        'email' => ''
+    ],
+    'profile_pic_type' => 'square' // square, circle, rectangle
+];
